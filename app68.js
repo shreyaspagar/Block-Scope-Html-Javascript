6ccc4790eb71ecf931d6b88d127453037297e596
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-05-08 13:16:14 +0530

    Add files via upload

---
+++ app68.js
@@ -0,0 +1,18 @@
+// {
+//     let a  = 25;
+// }
+
+// console.log(a);
+
+// for(let i = 1;i<=5;i++) {
+//     console.log(i);
+// }
+
+// console.log(i)
+
+let age = 25;
+if(age>=18) {
+    let str = "adult";
+    console.log(str);
+}
+
